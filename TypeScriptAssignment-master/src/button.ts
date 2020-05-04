fd1ba379c6c2d0127fd2ad8a348931b61970c4cb
Superscoti <57096064+Superscoti@users.noreply.github.com>
2020-05-04 16:43:59 +0200

    fixed getting array

--- TypeScriptAssignment-master/src/button.ts
+++ TypeScriptAssignment-master/src/button.ts
@@ -1,14 +1,31 @@
-export class buttonClass {
+import {questions} from './array';
+
+export default class buttonClass {
+
+    private question = document.getElementById("question")! as HTMLElement;
+    private index:number = 0;
+
+
     constructor() {
-        //import {questions} from './array';
-
-        var button = document.querySelector("#nextquestion");
-        var question = document.getElementById("question")! as HTMLElement;
-        
-        button?.addEventListener("click", function(){
-        question.innerHTML = "josef";
-        })
+        let button = document.querySelector("#nextquestion");
+        button?.addEventListener("click", ()=> {
+            console.log(this.index);
+            this.onClick();
+        });
     }
+
+    private nextIndex():void {
+        if(this.index >= questions.length-1) {
+            this.index = 0;
+        }else{
+            this.index += 1;
+        }
+    }
+
+    public onClick():void {
+        //console.log(questions[this.index]);
+        this.question.innerHTML = questions[this.index];
+        this.nextIndex();
+    }
+
 }
-
-
--- TypeScriptAssignment-master/src/index.ts
+++ TypeScriptAssignment-master/src/index.ts
@@ -1,11 +1,9 @@
 import { helloWorld, Beispiel } from './myModule';
 import { alertMe } from './myOtherModule';
 import {questions} from './array';
-//import {getButtonOnClick} from './button';
-import {buttonClass} from './button';
+import buttonClass from './button';
 
-var getbutton = new buttonClass;
-getbutton;
+new buttonClass;
 
 console.log(helloWorld);
 customElements.define('my-beispiel', Beispiel)
