6b35db057e52b0baf5acd88496dbd97c946387c8
Superscoti <57096064+Superscoti@users.noreply.github.com>
2020-05-04 12:05:19 +0200

    created array

--- TypeScriptAssignment-master/src/index.ts
+++ TypeScriptAssignment-master/src/index.ts
@@ -1,5 +1,6 @@
 import { helloWorld, Beispiel } from './myModule';
 import { alertMe } from './myOtherModule';
+import {questions} from './array';
 
 console.log(helloWorld);
 customElements.define('my-beispiel', Beispiel)
